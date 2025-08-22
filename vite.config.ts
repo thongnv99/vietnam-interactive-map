1ad07c1993ce6ffd71540938670600bb036c1203
thongnee <thong.nv173390@gmail.com>
2025-08-22 23:48:21 +0700

    fix build github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/demo-map/',
+  base: '/vietnam-interactive-map/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
